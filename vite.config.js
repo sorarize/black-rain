0e2f62667e732c2a8afc12aaf5e6b39be7c003a0
19 <sorarize@gmail.com>
2025-01-05 23:54:09 +0800

    server for mobile test

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
+    host: true,  // 自動檢測網路介面
     open: false
   },
   resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+    preserveSymlinks: true
   }
 })
